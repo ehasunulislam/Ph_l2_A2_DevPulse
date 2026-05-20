354dc3b8de58e84a7cd111ed47123eabc8f59dad
ehasun15019 <ehasunulislam0202@gmail.com>
2026-05-21 01:37:32 +0600

    database connected successfylly

---
+++ src/db/index.db.ts
@@ -0,0 +1,29 @@
+import { Pool } from "pg";
+import config from "../config/index.config.js";
+
+export const pool = new Pool({
+    connectionString: config.connection_string
+});
+
+const initDB = async () => {
+    try{
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(20),
+                email VARCHAR(20) UNIQUE NOT NULL,
+                password TEXT,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+         `)
+
+        console.log("Database connected successfully!");
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+
+export default initDB
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app.js"
 import config from "./config/index.config.js"
+import initDB from "./db/index.db.js"
 
 const main = () => {
+    initDB()
     app.listen(config.port, ()=> {
         console.log(`server is running on port ${config.port}`);
         
